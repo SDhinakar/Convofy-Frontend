bc3c722dc21c7214613f0e40ac6251ca8537b962
Dhinakar S <dhinakarshanmugam6@gmail.com>
2025-06-30 17:33:53 +0530

    updated the website's deployment name

--- src/components/NoChatSelected.jsx
+++ src/components/NoChatSelected.jsx
@@ -16,8 +16,9 @@ const NoChatSelected = () => {
           </div>
         </div>
 
+
         {/* Welcome Text */}
-        <h2 className="text-2xl font-bold">Welcome to Chatty!</h2>
+        <h2 className="text-2xl font-bold">Welcome to Convofyy!</h2>
         <p className="text-base-content/60">
           Select a conversation from the sidebar to start chatting
         </p>
